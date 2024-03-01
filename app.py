05f4c93cd2fc7f39352f1d2a13c8c9eaa77d283e
adthai <adthai@uw.edu>
2024-02-29 17:54:40 -0800

    upload profile images

--- app.py
+++ app.py
@@ -1,10 +1,57 @@
 from flask import Flask, session, render_template, request, flash, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, logout_user
+#from flask_wtf import FlaskForm
+#from wtforms import FileField, SubmitField
+import urllib.request
+from werkzeug.utils import secure_filename
+import os
+#from wtforms.validators import InputRequired
+
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test_nancy.db'
 app.config["SECRET_KEY"] = "abc"
+app.config['UPLOAD_FOLDER'] = 'static/uploads/'
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+
+# class UploadFileForm(FlaskForm):
+#     file=FileField("File", validators=[InputRequired()])
+#     submit=SubmitField("Upload File")
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/')
+def index():
+     return render_template('index.html')
+
+@app.route('/home', methods=['POST'])
+def upload_image():
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        flash('No image selected for uploading')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        #print('upload_image filename: ' + filename)
+        flash('Image successfully uploaded and displayed below')
+        return render_template('profile.html', filename=filename)
+    else:
+        flash('Allowed image types are - png, jpg, jpeg, gif')
+        return redirect(request.url)
+
+@app.route('/display/<filename>')
+def display_image(filename):
+    #print('display_image filename: ' + filename)
+    return redirect(url_for('static', filename='uploads/' + filename), code=301)
 
 db = SQLAlchemy(app)
 
@@ -36,10 +83,6 @@ class SonnyItems(UserMixin, db.Model):
     def __repr__(self):
         return f'<SonnyItems {self.name}>'
 
-
-@app.route('/')
-def index():
-    return render_template('index.html')
 
 
 @app.route('/profile')
@@ -91,6 +134,10 @@ def robbie():
 @app.route('/favorites')
 def favorites():
     return render_template('favorites.html')
+
+# @app.route('/get_name', methods=['GET'])
+# def get_name():
+
 
 
 @app.route('/add_inventory', methods=['POST'])
