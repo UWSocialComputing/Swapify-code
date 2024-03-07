97e33fb6c0912b6d875b501437c6372b9a999063
mxdxn <medinora@uw.edu>
2024-03-07 13:30:26 -0800

    Javascript for buttons is now in its own file & trade button immediately updates  when toggled

--- static/script/profile.js
+++ static/script/profile.js
@@ -1,41 +1,43 @@
-//for editing
-document.addEventListener('DOMContentLoaded', () => {
+function setupEventListeners() {
+  // For editing
   const editButtons = document.querySelectorAll('.editButton');
-
   editButtons.forEach(button => {
-      button.addEventListener('click', function () {
-          const targetFormId = this.getAttribute('data-target-form');
-          const targetForm = document.getElementById(targetFormId);
-          if (targetForm.style.display === 'none' || targetForm.style.display === '') {
-              targetForm.style.display = 'block';
-          }
-      });
+    button.addEventListener('click', function () {
+      const targetFormId = this.getAttribute('data-target-form');
+      const targetForm = document.getElementById(targetFormId);
+      if (targetForm.style.display === 'none' || targetForm.style.display === '') {
+        targetForm.style.display = 'block';
+      }
+    });
   });
-});
 
-//for trading
-document.querySelectorAll('.tradeButton').forEach(button => {
-  button.addEventListener('click', function () {
+  // For trading
+  document.querySelectorAll('.tradeButton').forEach(button => {
+    button.addEventListener('click', function () {
       const itemId = this.getAttribute('data-item-id');
       const itemImages = document.querySelectorAll(`.item-${itemId}`);
       itemImages.forEach(image => {
-          if (image.classList.contains('darkened')) {
-              image.classList.remove('darkened');
-          } else {
-              image.classList.add('darkened');
-          }
+        if (image.classList.contains('darkened')) {
+          image.classList.remove('darkened');
+        } else {
+          image.classList.add('darkened');
+        }
       });
-      // Send a request to the server to update the trade status
+
       fetch(`/toggle_trade/${itemId}`, {method: 'POST'})
-          .then(response => {
-              if (response.ok) {
-                  location.reload();
-              } else {
-                  console.error('Failed to update trade status');
-              }
-          })
-          .catch(error => {
-              console.error('Error:', error);
-          });
+        .then(response => {
+          if (response.ok) {
+            location.reload();
+          } else {
+            console.error('Failed to update trade status');
+          }
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        });
+    });
   });
-});
+}
+
+document.addEventListener('DOMContentLoaded', setupEventListeners);
+window.onload = setupEventListeners;
